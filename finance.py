36f1d1b2367e809cddc4a6eaa9cf24a309c14130
Dmitry Borisenko <dmitrijborisenko@gmail.com>
2017-05-01 10:56:09 +0200

    broomstick

--- finance.py
+++ finance.py
@@ -5,6 +5,7 @@ from pandas.tseries.offsets import DateOffset, MonthBegin, MonthEnd, \
     relativedelta
 from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
+import matplotlib.lines as mlines
 import foolbox.data_mgmt.set_credentials as set_credentials
 import pickle
 from foolbox.portfolio_construction import multiple_timing
@@ -12,6 +13,7 @@ from foolbox.portfolio_construction import multiple_timing
 
 my_red = "#ce3300"
 my_blue = "#2f649e"
+my_gray = "#8c8c8c"
 
 plt.rc("font", family="serif", size=12)
 
@@ -604,6 +606,64 @@ def pe_backtest(returns, holding_range, threshold_range,
     results["aggr"] = aggr
 
     return results
+
+
+def broomstick_plot(data, ci=(0.1, 0.9)):
+    """Given an input array of data, produces a broomstick plot, with all
+    series in gray, the mean of the series in black (along with confidence
+    interval). The data are intended to be cumulative returns on a set of
+    trading strategies
+
+    Parameters
+    ----------
+    data: pd.DataFrame
+        of the cumulative returns to plot
+    ci: tuple
+        of floats specifying lower and upper quantiles for empirical confidence
+        interval. Default is (0.1, 0.9)
+
+    Returns
+    -------
+    figure: matplotlib.pyplot.plot
+        with plotted data
+
+
+    """
+    # Check the input data
+    if not isinstance(data, pd.DataFrame):
+        raise TypeError("Nope, the input should be a DataFrame")
+
+    # Drop absent observations if any
+    data = data.dropna()
+
+    # Get the mean and confidence bounds
+    cb_u = data.quantile(ci[1], axis=1)
+    cb_l = data.quantile(ci[0], axis=1)
+    avg = data.mean(axis=1)
+    stats = pd.concat([avg, cb_u, cb_l], axis=1)
+    stats.columns = ["mean", "cb_u", "cb_l"]
+
+    # Start plotting
+    fig, ax = plt.subplots(figsize=(8.4, 11.7/3))
+    plt.setp(ax.get_xticklabels(), rotation=0, ha="center")
+    # Grid
+    ax.grid(which="both", alpha=0.33, linestyle=":")
+    ax.plot(data, color=my_gray, lw=0.75, alpha=0.25)
+
+    ax.plot(stats["mean"], color="k", lw=2)
+    ax.plot(stats[["cb_l", "cb_u"]], color="k", lw=2, linestyle="--")
+
+    # Construct lines for the custom legend
+    solid_line = mlines.Line2D([], [], color='black', linestyle="-",
+                               lw=2, label="Mean")
+
+    ci_label = "{}th and {}th percentiles"\
+        .format(int(ci[0]*100), int(ci[1]*100))
+    dashed_line = mlines.Line2D([], [], color='black', linestyle="--",
+                                lw=2, label=ci_label)
+    ax.legend(handles=[solid_line, dashed_line], loc="upper left")
+
+    return fig
 
 
 if __name__  == "__main__":
