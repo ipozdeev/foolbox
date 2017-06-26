fc4c8959410263d7478106d8d16dde2c53af0556
ipozdeev <igor.pozdeev@unisg.ch>
2017-06-26 14:51:24 +0200

    added wp_settings, cosmetic change

--- api.py
+++ api.py
@@ -3,9 +3,13 @@ from foolbox.EventStudy_new import *
 import foolbox.portfolio_construction as poco
 import foolbox.tables_and_figures as taf
 from foolbox.finance import *
+import pickle
+from foolbox.wp_tabs_figs.wp_settings import *
+
 import foolbox.data_mgmt.set_credentials as set_credentials
 set_credentials.set_r_environment()
 data_path = set_credentials.gdrive_path("research_data/fx_and_events/")
-import pickle
 
-print("imported pickle, poco, taf, EventStudy, data_path. Set envir for R.")
+
+print("imported pickle, plt, poco, taf, EventStudy, data_path." +
+    "\nSet envir for R and matplotlib settings.")
