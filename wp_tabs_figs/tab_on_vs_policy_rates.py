64341f0a0f03adbdc8f9e91148b9fcb0a315b245
ipozdeev <igor.pozdeev@unisg.ch>
2017-08-20 12:16:29 +0200

    boxplot showing unbiasedness, regression tab started

--- wp_tabs_figs/tab_on_vs_policy_rates.py
+++ wp_tabs_figs/tab_on_vs_policy_rates.py
@@ -1,56 +1,133 @@
-from foolbox.api import *
 import pandas as pd
-from foolbox.RegressionModel import DynamicOLS
-import ipdb
+from matplotlib.ticker import FixedLocator
+from matplotlib import lines as mlines, patches as mpatches
+import seaborn as sns
+
+from foolbox.api import *
+from foolbox.RegressionModel import light_ols
+from foolbox.wp_tabs_figs.wp_settings import *
+
+def fig_implied_rates_unbiasedness(tgt_rate_diff, on_rate_diff):
+    """
+    """
+    # rename, just in case
+    y = on_rate_diff.rename("on")
+    x = tgt_rate_diff.rename("tgt")
+
+    # concatenate, drop na
+    data_to_boxplot = pd.concat((x, y), axis=1).dropna()
+
+    # unique target rate changes; need sorted because will plot on text axis
+    unq_tgt_change = sorted(data_to_boxplot["tgt"].unique())
+
+    # start figure
+    fig, ax = plt.subplots()
+
+    # boxplots --------------------------------------------------------------
+    bp = sns.boxplot(data=data_to_boxplot, x="tgt", y="on",
+        color=new_blue, saturation=.9, fliersize=4, ax=ax)
+
+    # plot bisector-like points ---------------------------------------------
+    # (45 degree line showing unbiasedness)
+    for p in range(len(unq_tgt_change)):
+        ax.scatter(p, unq_tgt_change[p], marker='o',
+            color=new_red, edgecolor='k', s=90)
+
+    # number of cases -------------------------------------------------------
+    # in a gray box below
+    # nee ylim to determine the 'below'
+    ylim = ax.get_ylim()
+
+    cnt = 0
+    for p, q in data_to_boxplot.groupby("tgt"):
+        ax.annotate(str(q.on.count()), xy=(cnt, ylim[0]),
+            fontsize=12, bbox=dict(facecolor=new_gray, edgecolor='k'),
+            horizontalalignment='center', verticalalignment='center')
+        cnt += 1
+
+    # artists ---------------------------------------------------------------
+    # grid
+    bp.grid(axis='y', linestyle=':', color='k', alpha=0.45)
+    plt.setp(bp.artists, alpha=.90)
+
+    # limits
+    ax.yaxis.set_major_locator(FixedLocator(np.arange(
+        np.ceil(ax.get_ylim()[0]/0.25)*0.25,
+        np.ceil(ax.get_ylim()[-1]/0.25)*0.25,
+        0.25)))
+    ax.set_ylim((ylim[0]-0.05, ylim[-1]+0.05))
+
+    # ticks
+    bp.tick_params(axis='y', labelright=True)
+    bp.tick_params(axis='both', labelsize=12)
+
+    # labels
+    ax.set_xlabel('target rate', fontsize=12)
+    ax.set_ylabel('o/n rate', fontsize=12)
+    bp.yaxis.label.set_size(12)
+
+    # legend
+    solid_line = mlines.Line2D([], [], color=new_red, linestyle="none",
+        marker='o', markersize=10,
+        label=r"($\alpha=0$, $\beta=1$) points")
+    gray_patch = mpatches.Patch(color=new_gray, label="number of cases")
+
+    ax.legend(handles=[solid_line, gray_patch], loc='upper right',
+        bbox_to_anchor=(0.35, 0.9), fontsize=12)
+
+    return fig
 
 if __name__ == "__main__":
 
     # data ------------------------------------------------------------------
-    # overniht rates
+    # overnight rates
     with open(data_path + "ois_bloomberg.p", mode="rb") as hangar:
         ois_data = pickle.load(hangar)
 
     on_rates = pd.concat(
         [p.loc[:, "ON"].to_frame(c) for c, p in ois_data.items()],
-        axis=1)
+        axis=1).astype(float)
 
     # policy rates
     with open(data_path + "events.p", mode="rb") as hangar:
         events_data = pickle.load(hangar)
 
-    tgt_rate_changes = events_data["joint_cbs"]
+    tgt_rate_changes = events_data["joint_cbs"].astype(float)
 
-    # lag settings
-    all_settings = {
-        "aud": 1,
-        "cad": 0,
-        "chf": 0,
-        "eur": 1,
-        "gbp": 1,
-        "nzd": 0,
-        "sek": 1,
-        "usd": 1
-        }
+    # # lag settings
+    # all_settings = {
+    #     "aud": 1,
+    #     "cad": 0,
+    #     "chf": 0,
+    #     "eur": 1,
+    #     "gbp": 1,
+    #     "nzd": 0,
+    #     "sek": 1,
+    #     "usd": 1
+    #     }
 
-    # rolling regression ----------------------------------------------------
-    roll_betas = dict()
+    # OLS regression --------------------------------------------------------
+    coef = dict()
+    se = dict()
 
-    for c, lag in all_settings.items():
+    for c in tgt_rate_changes.columns:
         # c = "usd"
-        # lag = 1
-        this_on_rate_change = on_rates.loc[:, c].diff()
+        this_ois = OIS.from_iso(c, DateOffset(months=1))
+
         this_tgt_rate_change = tgt_rate_changes.loc[:, c].dropna()
+        this_on_rate = on_rates.loc[:, c]
+        this_on_rate_avg = this_ois.get_rates_until(this_on_rate,
+            meetings=this_tgt_rate_change,
+            method="average")
+        this_on_rate_diff = (
+            this_on_rate_avg.shift(-10).loc[this_tgt_rate_change.index] -
+            this_on_rate_avg.shift(5).loc[this_tgt_rate_change.index])
 
-        y0 = this_on_rate_change.copy()
-        # y0.name = "on"
-        x0 = this_tgt_rate_change.shift(-lag)
-        # x0.name = "tgt"
-        ipdb.set_trace()
-        dynamic_mod = DynamicOLS("expanding",
-            y0=y0,
-            x0=x0,
-            min_periods=10)
-        this_b = dynamic_mod.fit()
+        # regression
+        y0 = this_on_rate_diff.rename("on")
+        x0 = this_tgt_rate_change.rename("tgt")
 
-        lol = pd.concat((y0,x0), axis=1).expanding(min_periods=10).cov()
-        lol.loc[:,"on","tgt"]
+        this_b, _, this_se = light_ols(y0, x0, add_constant=True, ts=True)
+
+        coef[c] = this_b
+        se[c] = this_se
