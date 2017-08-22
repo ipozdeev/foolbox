298bbf51245fd499f9a6b05306ac184e85009fd8
ipozdeev <igor.pozdeev@unisg.ch>
2017-08-22 23:33:41 +0200

    fixed index calculation (skip every second row) and typo with coef/tstat

--- utils.py
+++ utils.py
@@ -504,7 +504,7 @@ def to_better_latex(df_coef, df_tstat, fmt_coef="{}", fmt_tstat="{}",
     mask_coef = df_coef.applymap(np.isfinite)
     mask_tstat = df_tstat.applymap(np.isfinite)
 
-    df_coef_fmt = df_tstat.applymap(fmt_tstat.format)
+    df_coef_fmt = df_coef.applymap(fmt_tstat.format)
     df_tstat_fmt = df_tstat.applymap(('('+fmt_tstat+')').format)
 
     new_df = []
@@ -518,7 +518,7 @@ def to_better_latex(df_coef, df_tstat, fmt_coef="{}", fmt_tstat="{}",
         new_mask += [q]
 
     new_idx = []
-    for p, q in zip(list(df_coef.index), [' ', ' ']):
+    for p, q in zip(list(df_coef.index), [' ',]*len(df_coef.index)):
         new_idx += [p]
         new_idx += [q]
 
@@ -531,7 +531,9 @@ def to_better_latex(df_coef, df_tstat, fmt_coef="{}", fmt_tstat="{}",
 
     return new_df.to_latex(**kwargs)
 
+
 if __name__ == "__main__":
+    pass
     # from foolbox.api import *
     # fname = data_path + "ois_2000_2017_d.xlsx"
     # lol = parse_bloomberg_excel(
@@ -542,115 +544,5 @@ if __name__ == "__main__":
     # lol = pd.Panel.from_dict(all_data, orient="minor")
     # instr = lol.loc["1M",:,:]
 
-
-    from foolbox.api import *
-    # Get the data
-    with open(data_path + "ois_bloomberg.p", mode="rb") as halupa:
-        ois_data = pickle.load(halupa)
-
-    # Value date, day count convention and fixing lag settings. Note that US
-    # fixing lag is set to 0, since the overnight data (effr) is already lagged
-    # by the New York Fed
-    all_settings = {
-        "aud": {"start_date": 1,
-                "fixing_lag": 0,
-                "day_count_float": 365,
-                "date_rolling": "modified following"},
-
-        "cad": {"start_date": 0,
-                "fixing_lag": 1,
-                "day_count_float": 365,
-                "date_rolling": "modified following"},
-
-        "chf": {"start_date": 2,
-                "fixing_lag": -1,
-                "day_count_float": 360,
-                "date_rolling": "modified following"},
-
-        "eur": {"start_date": 2,
-                "fixing_lag": 0,
-                "day_count_float": 360,
-                "date_rolling": "modified following"},
-
-        "gbp": {"start_date": 0,
-                "fixing_lag": 0,
-                "day_count_float": 365,
-                "date_rolling": "modified following"},
-
-        "jpy": {"start_date": 2,
-                "fixing_lag": 1,
-                "day_count_float": 365,
-                "date_rolling": "modified following"},
-
-        "nzd": {"start_date": 2,
-                "fixing_lag": 0,
-                "day_count_float": 365,
-                "date_rolling": "modified following"},
-
-        "sek": {"start_date": 2,
-                "fixing_lag": -1,
-                "day_count_float": 360,
-                "date_rolling": "modified following"},
-
-        "usd": {"start_date": 2,
-                "fixing_lag": 0,
-                "day_count_float": 360,
-                "date_rolling": "modified following"}
-
-        }
-
-    test_currencies = ["aud", "cad", "chf", "eur", "gbp", "nzd", "sek", "usd"]
-    test_maturity = "1M"  # corresponds to column names in ois_data dataframes
-
-    diffs = dict()
-    for curr in test_currencies:
-        # Select the data
-        # curr = "usd"
-        test_curr = curr
-        test_data = ois_data[test_curr]
-        settings = all_settings[test_curr]
-
-        # Create the dataoffset object according to the desired maturity
-        if test_maturity[1] == "M":
-            maturity = DateOffset(months=int(test_maturity[0]))
-
-        if test_maturity[1] == "Y":
-            maturity = DateOffset(months=12*int(test_maturity[0]))
-
-        # Get the inputs for return estimation: trade dates first
-        trade_dates = test_data[test_maturity].dropna().index
-        # Return of the underlying floating rate
-        returns = test_data["ON"].dropna()
-
-        # Compute returns and periods over which the returns realized
-        realized_ret_data = \
-            compute_floating_leg_return(trade_dates, returns, maturity,
-                                        settings)
-
-        # Concatenate the results: fixed, start and end dates, realized
-        res = pd.concat([test_data[test_maturity], realized_ret_data["dates"],
-                         realized_ret_data["ret"]], join="inner", axis=1)
-        res.columns = ["fixed", "start", "end", "realized"]
-
-        # Print something
-        # print(curr, res.head(22))
-
-        # Plot fixed vs realized float
-        to_plot = res[["fixed", "realized"]].dropna()
-        # to_plot.plot(title=test_curr + "_" + test_maturity)
-
-        diffs[curr] = (to_plot.fixed - to_plot.realized).astype("float")
-
-    plt.show()
-
-    # Test the unbiasedness
-    diff = \
-        (to_plot.realized - to_plot.fixed).to_frame(name="d").astype("float")
-
-    lol = pd.DataFrame.from_dict(diffs)
-    taf.descriptives(diff.loc[:"2005"], 1)
-
-    diff.plot()
-
-
-    print(taf.descriptives(to_plot.diff(axis=1).iloc[:,[-1]].dropna(), 1))
+    # to_better_latex(coef*100, se*100, fmt_coef="{:3.2f}", fmt_tstat="{:3.2f}",
+        #     buf=out_path+"temp.tex", column_format="l"+"W"*len(se.columns))
