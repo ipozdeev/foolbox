c061f5219c79c7ba696fa43f41bc6807cdee7753
Dmitry Borisenko <dmitrijborisenko@gmail.com>
2017-12-05 17:38:25 +0100

    new parsing of ois data, parsing of yield data

---
+++ data_mgmt/ois_project/process_stock_vix_ted.py
@@ -0,0 +1,16 @@
+from foolbox.api import *
+
+file_to_parse = data_path + "msci_ted_vix_2000_2017.xlsm"
+out_name = "msci_ted_vix_d.p"
+
+# Parse the data
+raw_data = pd.read_excel(file_to_parse, sheetname=None)
+
+# Save the sheets and variables needed
+out_data = dict()
+out_data["msci_pi"] = raw_data["msci"]
+out_data["ted_vix"] = raw_data["ted_vix"][["ted", "vix"]]
+
+# Dump
+with open(data_path + out_name, mode="wb") as halupa:
+    pickle.dump(out_data, halupa)
