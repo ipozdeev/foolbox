fcba0bd6e3f8f3ed24d39b5b6001f01fca0b3260
ipozdeev <igor.pozdeev@unisg.ch>
2017-12-22 14:31:40 +0100

    minor changes

--- fxtrading.py
+++ fxtrading.py
@@ -37,7 +37,7 @@ class StrategyFactory():
             pos_weights = position_flags.divide(row_lev, axis=0)
 
         elif method == "net":
-            # delverage positive and negative positions separately
+            # deleverage positive and negative positions separately
             row_lev_pos = position_flags.where(position_flags > 0)\
                 .apply(axis=1, func=np.nansum)
             row_lev_neg = -1*position_flags.where(position_flags < 0)\
@@ -107,12 +107,12 @@ class FXTradingStrategy():
         self._position_weights = value
 
     @classmethod
-    def from_position_flags(cls, position_flags, leverage="none"):
+    def from_position_flags(cls, position_flags, leverage="net"):
         """
         leverage : str
-            "full" for allowing unlimited leverage
-            "none" for restricting it to 1
-            "net" for restricting that short and long positions net out to
+            "unlimited" for allowing unlimited leverage
+            "net" for restricting it to 1
+            "zero" for restricting that short and long positions net out to
                 no-leverage
         """
         pos_weights = StrategyFactory().deleverage_position_flags(
@@ -139,9 +139,9 @@ class FXTradingStrategy():
         hold_period : int
             the number of periods to maintain position
         leverage : str
-            "full" for allowing unlimited leverage
-            "none" for restricting it to 1
-            "net" for restricting that short and long positions net out to
+            "unlimited" for allowing unlimited leverage
+            "net" for restricting it to 1
+            "zero" for restricting that short and long positions net out to
                 no-leverage
         """
         # I. position flags -------------------------------------------------
@@ -156,10 +156,10 @@ class FXTradingStrategy():
         position_flags = cont_events.shift(-blackout)
 
         # make sure there is one nan at the beginning to be able to open a pos
-        if position_flags.iloc[:hold_period,:].notnull().any().any():
+        if position_flags.iloc[:hold_period, :].notnull().any().any():
             warnings.warn("There are not enough observations at the start; " +
                 "will delete the first events")
-            position_flags.iloc[:hold_period,:] = np.nan
+            position_flags.iloc[:hold_period, :] = np.nan
 
         # NB: dimi/
         # # Wipe the intruders who have events inside the first holding period
@@ -269,6 +269,8 @@ class FXTradingEnvironment():
         """
         self.spot_prices = spot_prices
         self.swap_points = swap_points
+
+        self.currencies = spot_prices.minor_axis
 
     @classmethod
     def from_scratch(cls, spot_prices, swap_points=None):
@@ -896,8 +898,7 @@ if __name__ == "__main__":
     base_th = settings["base_threshold"]
 
     # carry
-    with open(data_path+"fx_by_tz_sp_fixed.p", mode="rb") as hangar:
-        data_d = pickle.load(hangar)
+    data_d = pd.read_pickle(data_path+"fx_by_tz_sp_fixed.p")
 
     fx_tr_env = FXTradingEnvironment.from_scratch(
         spot_prices={
@@ -908,7 +909,7 @@ if __name__ == "__main__":
             "ask": data_d["tnswap_ask"].loc[:, start_date:, "NYC"]}
             )
 
-    fx_tr_env.drop(labels=drop_curs, axis="minor_axis", errors="ignore")
+    fx_tr_env.drop(labels=["dkk"], axis="minor_axis", errors="ignore")
     fx_tr_env.remove_swap_outliers()
     fx_tr_env.reindex_with_freq('D')
     fx_tr_env.align_spot_and_swap()
@@ -917,13 +918,13 @@ if __name__ == "__main__":
     fdisc_d = fx_tr_env.swap_points.mean(axis="items")
     spot_d = fx_tr_env.spot_prices.mean(axis="items")
 
-    dr = -1*np.log((fdisc_d + spot_d)/spot_d).rolling(22).mean()
+    # dr = -1*np.log((fdisc_d + spot_d)/spot_d).rolling(22).mean()
 
-    fx_tr_str_carry = FXTradingStrategy.monthly_from_daily_signals(
-        signals_d=dr, n_portf=3, leverage="net")
+    # fx_tr_str_carry = FXTradingStrategy.monthly_from_daily_signals(
+    #     signals_d=dr, n_portf=3, leverage="net")
 
     # ois-implied forecasts
-    curs = [p for p in dr.columns if p not in ["dkk", "nok", "jpy"]]
+    curs = [p for p in spot_d.columns if p not in ["dkk", "nok", "jpy"]]
 
     # forecast direction
     signals_fcast = get_pe_signals(curs, base_lag, base_th*100, data_path,
@@ -932,18 +933,19 @@ if __name__ == "__main__":
         avg_refrce_over=avg_refrce_over,
         bday_reindex=True)
 
-    signals_fomc = get_pe_signals(curs, base_lag, base_th*100, data_path,
+    signals_fomc = get_pe_signals(curs + ["nok", "jpy"],
+        base_lag, base_th*100, data_path,
         fomc=True,
         avg_impl_over=avg_impl_over,
         avg_refrce_over=avg_refrce_over,
         bday_reindex=True)
 
+    signals_fcast.loc[:, "nok"] = np.nan
+    signals_fcast.loc[:, "jpy"] = np.nan
+
     signals_fcast, signals_fomc = signals_fcast.align(signals_fomc, axis=0,
         join="outer")
     signals_fcast = signals_fcast.fillna(signals_fomc)
-
-    signals_fcast.loc[:, "nok"] = np.nan
-    signals_fcast.loc[:, "jpy"] = np.nan
 
     signals_fcast = signals_fcast.loc[start_date:end_date].reindex(
         index=pd.date_range(start_date, end_date, freq='B'))
@@ -951,16 +953,16 @@ if __name__ == "__main__":
     strategy_fcast = FXTradingStrategy.from_events(signals_fcast,
         blackout=1, hold_period=10, leverage="net")
 
-    combined_strat = fx_tr_str_carry + strategy_fcast
+    # combined_strat = fx_tr_str_carry + strategy_fcast
 
-    fx_tr = FXTrading(environment=fx_tr_env, strategy=fx_tr_str_carry)
+    # fx_tr = FXTrading(environment=fx_tr_env, strategy=fx_tr_str_carry)
     fx_tr = FXTrading(environment=fx_tr_env, strategy=strategy_fcast)
-    fx_tr = FXTrading(environment=fx_tr_env, strategy=combined_strat)
+    # fx_tr = FXTrading(environment=fx_tr_env, strategy=combined_strat)
 
-    car = fx_tr.backtest("unrealiz")
+    # car = fx_tr.backtest("unrealiz")
     strat = fx_tr.backtest("unrealiz")
 
-    car.dropna().plot()
+    # car.dropna().plot()
     strat.dropna().plot()
     strat = strat.loc[
         strategy_fcast.position_flags.dropna(how="all").index]
