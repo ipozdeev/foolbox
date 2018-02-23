2511439db5d90557dfe32a0b68078e999de0a1f3
ipozdeev <igor.pozdeev@unisg.ch>
2018-02-22 21:36:55 -0500

    add garch, arima (started)

--- econometrics.py
+++ econometrics.py
@@ -1,142 +1,305 @@
-#
 import itertools
-import logging
 import string
 import warnings
+import logging
 
 import numpy as np
 import pandas as pd
 import statsmodels.api as sm
-
-logging.basicConfig(level=logging.INFO)
+from statsmodels.tsa import arima_model
+from scipy import signal
 
 import foolbox.data_mgmt.set_credentials as set_credentials
-set_credentials.set_r_environment()
+
 
 import rpy2.robjects as robj
 from rpy2.robjects.packages import importr
 from rpy2.robjects import Formula
-
 from rpy2.robjects import pandas2ri
-pandas2ri.activate()
-
 from rpy2.robjects import numpy2ri
+
+pandas2ri.activate()
 numpy2ri.activate()
 
-class Garch():
+logging.basicConfig(level=logging.INFO)
+
+set_credentials.set_r_environment()
+
+class ARIMA:
+    """ARIMA model.
+
+    Parameters
+    ----------
+    data
+    order
+    kwargs
+
     """
+    def __init__(self, data, order, **kwargs):
+        """
+        """
+        self.order = order
+        self.data = data.copy()
+
+        self.model = arima_model.ARIMA(endog=data, order=order, **kwargs)
+
+        self._fitted = None
+
+    def fit(self, **kwargs):
+        """
+
+        Parameters
+        ----------
+        kwargs
+
+        Returns
+        -------
+
+        """
+        fitted = self.model.fit(**kwargs)
+
+        self._fitted = fitted
+
+        return
+
+    def predict(self, out_of_sample=False, **kwargs):
+        """
+
+        Parameters
+        ----------
+        out_of_sample
+        kwargs
+
+        Returns
+        -------
+
+        """
+        if out_of_sample:
+            pass
+
+        res = self._fitted.predict(**kwargs)
+
+        return res
+
+
+class GARCH:
+    """ARMA-GARCH model.
+
     Model of the form:
-    y_t = mu + ar_1*y_{t-1} + ... + ma_1*eps_{t-1} + eps_t (1)
-    eps_t ~ d(0,sigma_t)
-    sigma^2_t = omega + alpha_1*eps_{t-1}^2 + ... + beta_1*sigma^2_{t-1} (2)
+
+        y_t = mu + ar_1*y_{t-1} + ... + ma_1*eps_{t-1} + eps_t (1)
+        eps_t ~ d(0,sigma_t)
+        sigma^2_t = omega + alpha_1*eps_{t-1}^2 + ... + beta_1*sigma^2_{t-1} (2)
+
     Such that the mean equation (1) is an ARMA process, and the variance
     equation (2) is a GARCH() process.
 
     Parameters
     ----------
-    P: int
+    garch_lags: int
         number of ARCH lags in variance equation
-    Q: int
+    garch_lags: int
         number of GARCH lags in variance equation
-    AR: int
+    ar_lags: int
         autoregressive component of mean equation
-    MA: int
+    ma_lags: int
         moving average component of mean equation
     """
-    def __init__(self, P, Q, AR = 0, MA = 0):
-        # import some R packages
-        self.r_base   = importr("base")
+    def __init__(self, arch_lags, garch_lags, ar_lags=0, ma_lags=0):
+        """
+        """
+        # import R packages
+        self.r_base = importr("base")
         self.r_fGarch = importr("fGarch")
-        self.r_stats  = importr("stats")
+        self.r_stats = importr("stats")
 
         # model specs
-        self.P        = P
-        self.Q        = Q
-        self.AR       = AR
-        self.MA       = MA
+        self.arch_lags = arch_lags
+        self.garch_lags = garch_lags
+        self.ar_lags = ar_lags
+        self.ma_lags = ma_lags
+
+        # assign coefficient names: omega, ar1,...,ma1,...,alpha1,...,beta1,...
+        self.ar_names = ["ar" + str(n + 1) for n in range(ar_lags)]
+        self.ma_names = ["ma" + str(n + 1) for n in range(ma_lags)]
+        self.arch_names = ["alpha" + str(n + 1) for n in range(arch_lags)]
+        self.garch_names = ["beta" + str(n + 1) for n in range(garch_lags)]
 
         # R formula of the equation
-        self.formula  = Formula(
-            "~arma({ar:1d},{ma:1d})+garch({p:1d},{q:1d})".\
-            format(ar=self.AR,ma=self.MA,p=self.P,q=self.Q)
-            )
+        self.formula = Formula(
+            "~arma({ar:1d},{ma:1d})+garch({p:1d},{q:1d})".
+            format(ar=self.ar_lags, ma=self.ma_lags,
+                   p=self.arch_lags, q=self.garch_lags)
+        )
 
-
-    def fit(self, data, include_mean = True):
-        """
-        Estimates parameters of the model.
+    def fit(self, data, include_mean=True):
+        """Estimate model parameters.
 
         Parameters
         ----------
-        data: pd.Series
+        data : pandas.Series
             series of values
-        include_mean: Boolean
-            whether the mean will be estimated or not
+        include_mean : bool
+            whether the unsonditional mean will be estimated or not
 
         Returns
         -------
-        coef: dict
+        coef : dict
             coefficients
-        h: pd.Series
+        h : pandas.Series
             conditional variances, with the same index as *data*
 
         """
+        # clean up data
+        data_clean = data.dropna()
         # data to R dataframe
-        self.rdf = pandas2ri.py2ri(data.dropna()) # TODO: work around dropna?
+        rdf = pandas2ri.py2ri(data_clean)
 
         # fit GARCH
-        self.g = self.r_fGarch.garchFit(
-            formula = self.formula,
-            data    = self.rdf,
-            trace   = False,
-            include_mean = include_mean)
+        fitted = self.r_fGarch.garchFit(formula=self.formula, data=rdf,
+                                        trace=False, include_mean=include_mean)
 
-        # fetch coefficients from model output
-        coef = pandas2ri.ri2py(self.g.slots['fit'][0])
+        fitted.data_orig = data_clean
 
-        # assign coefficient names: omega, ar1,...,ma1,...,alpha1,...,beta1,...
-        ar_names  = ["ar"+str(n) for n in range(1, self.AR+1)]
-        ma_names  = ["ma"+str(n) for n in range(1, self.MA+1)]
-        p_names   = ["alpha"+str(n) for n in range(1, self.P+1)]
-        q_names   = ["beta"+str(n) for n in range(1, self.Q+1)]
-        all_names = ar_names + ['omega'] + ma_names + p_names + q_names
+        return fitted
 
-        # if mean is there, prepend it
-        if include_mean:
-            all_names = ["mu",] + all_names
+    def get_h(self, fitted):
+        """
 
-        # store coefficients as a dict with the above names
-        self.coef = dict(zip(all_names, coef))
+        Parameters
+        ----------
+        fitted
+
+        Returns
+        -------
+
+        """
+        coef = self.get_coef(fitted)
 
         # conditional variances
-        h = pandas2ri.ri2py(self.g.slots['h.t'])
-        self.h = pd.Series(data = h, index = data.dropna().index)
+        h = pandas2ri.ri2py(fitted.slots['h.t'])
+        h = pd.Series(data=h, index=fitted.data_clean.index)
+
+        return h
+
+    def predict(self, fitted, n_ahead=1):
+        """
+
+        Parameters
+        ----------
+        fitted
+        n_ahead
+
+        Returns
+        -------
+
+        """
+        coef = self.get_coef(fitted)
+        s2_hat_1 = self._predict_one(fitted)
+
+        if n_ahead < 2:
+            return s2_hat_1
+
+        else:
+            res = pd.Series(index=range(1, n_ahead+1))
+            res.loc[1] = s2_hat_1
+
+            # for t in range(2, n_ahead+1):
+            #     res.loc[t] = coef.loc["omega"] +\
+            #                  (coef.loc["alpha1"] + coef.loc["beta1"]) *\
+            #                  res.loc[t-1]
+
+            x = np.ones((n_ahead-1, ))*coef["omega"]
+            b = [1, ]
+            a = [1, -coef.loc[["alpha1", "beta1"]].sum()]
+            zi = signal.lfiltic(b, a, [s2_hat_1])
+
+            res.loc[2:] = signal.lfilter(b, a, x, zi=zi)[0]
+
+            return res
+
+    def _predict_one(self, fitted):
+        """
+
+        Parameters
+        ----------
+        fitted
+
+        Returns
+        -------
+
+        """
+        # lagged conditional variances
+        h = pandas2ri.ri2py(fitted.slots['h.t'])[-self.garch_lags:]
+        h = pd.Series(data=h, index=self.garch_names)
+
+        # lagged squared residuals
+        eps = pandas2ri.ri2py(fitted.slots['residuals'])[-self.arch_lags:]**2
+        eps = pd.Series(data=eps, index=self.arch_names)
+
+        # 1 to be multiplied with omega
+        omega = pd.Series(data=[1], index=["omega"])
+
+        # all together
+        data = pd.concat((h, eps, omega))
+
+        res = self.get_coef(fitted).dot(data)
+
+        return res
+
+    def get_coef(self, fitted):
+        """
+
+        Parameters
+        ----------
+        fitted
+
+        Returns
+        -------
+
+        """
+        # fetch coefficients from model output
+        coef = pandas2ri.ri2py(fitted.slots['fit'][0])
+
+        # assign coefficient names: omega, ar1,...,ma1,...,alpha1,...,beta1,...
+        all_names = self.ar_names + ['omega'] + self.ma_names + \
+                    self.arch_names + self.garch_names
+
+        # if mean is there, prepend it
+        n_coef = self.ar_lags + self.ma_lags + self.arch_lags + self.garch_lags
+        if len(coef) > n_coef + 1:
+            all_names = ["mu", ] + all_names
+
+        # store coefficients as a dict with the above names
+        coef = pd.Series(dict(zip(all_names, coef)))
+
+        return coef
 
     def __str__(self):
         """
         String representation
         """
-        return("ARMA({ar:<1d},{ar:<1d})+GARCH({p:<1d},{q:<1d}) model".\
-            format(ar=self.AR, ma=self.MA, p=self.P,q=self.Q))
+        res = "ARMA({ar:<1d},{ar:<1d})+GARCH({p:<1d},{q:<1d}) model"\
+            .format(ar=self.ar_lags, ma=self.ma_lags,
+                    p=self.arch_lags, q=self.garch_lags)
+        return res
 
 
 def nyan(data):
-    """
-    Randomly inserts a kitten into *data*. Try to find it!!!
+    """Randomly insert a kitten into *data*. Try to find it!!!
 
     Parameters
     data: pd.DataFrame
         some data
 
-    ----------
     """
-    data.dtype = str
-    T,N = data.shape
+    data = data.astype(str)
+    n_obs, n_cols = data.shape
 
-    newT = int(np.random.uniform(size=1)*(T))
-    newN = int(np.random.uniform(size=1)*(N))
+    new_t = int(np.random.uniform(size=1)*n_obs)
+    new_n = int(np.random.uniform(size=1)*n_cols)
 
-    data.ix[newT,newN] = ">''<"
+    data.iloc[new_t, new_n] = ">''<"
 
     return data
 
@@ -385,7 +548,7 @@ def interpolateQ(data_q, data_m):
     """
     # merge monthly-aggregated-to-quarterly data and truly quarterly data
     mySum = lambda x: pd.DataFrame.sum(x, skipna = False)   # fun to sum w/o NA
-    data_q = pd.concat((data_m.resample('Q').apply(mySum), data_q), axis = 1)
+    data_q = pd.concat((data_m.resample('garch_lags').apply(mySum), data_q), axis = 1)
 
     # rename columns: a, b etc., then inf
     data_q.columns = \
@@ -416,7 +579,7 @@ def interpolateQ(data_q, data_m):
                              columns = ["inf"])
 
     # aggregate at quarterly frequency
-    inf_hat_q = inf_hat_m.resample('Q').apply(mySum)
+    inf_hat_q = inf_hat_m.resample('garch_lags').apply(mySum)
 
     # calculate errors between true quarterly and fitted-m-resampled-to-q
     err = data_q["inf"].to_frame().subtract(inf_hat_q, axis = 0)
@@ -429,7 +592,7 @@ def interpolateQ(data_q, data_m):
     inf_hat_m = inf_hat_m.add(err)
 
     # check if resampled quarterly are indeed quarterly
-    check = inf_hat_m.resample('Q').apply(mySum)
+    check = inf_hat_m.resample('garch_lags').apply(mySum)
     if not round(check.tail(), 5).equals(
         round(data_q["inf"].to_frame().tail(), 5)):
         warnings.warn("The resampled monthly interpolated data is not the " +
@@ -876,7 +1039,7 @@ def rolling_garch(y, window, min_periods=None):
 
         for t in np.arange(first_obs_idx+min_periods, last_obs_idx+1):
             temp_y = temp_series.ix[t-window+1:t]
-            model  = Garch(1,1,1,0)
+            model  = GARCH(1, 1, 1, 0)
             try:
                 model.fit(temp_y, include_mean = True)
                 h[name].ix[t] = model.h[-1]
@@ -891,6 +1054,7 @@ def rolling_garch(y, window, min_periods=None):
 
 
     return params, h
+
 
 def nw_cov(data):
     """Computes Newey-West variance-covariance matrix for a dataframe, with
