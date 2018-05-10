f0dac0c5893e2eb8830948450335f30f445efa15
Dmitry Borisenko <dmitrijborisenko@gmail.com>
2018-05-10 18:42:36 +0200

    added some comments

--- wp_tabs_figs/event_regressions.py
+++ wp_tabs_figs/event_regressions.py
@@ -16,6 +16,10 @@ if __name__ == "__main__":
     fixing_time = "LON"
     currs = ["aud", "cad", "chf", "eur", "gbp", "sek", "nzd"]
 
+    # Whether to include currency fixed effects or estimate panel ols with a
+    # single intercept for all currencies
+    include_currency_fe = False
+
     # OIS/ON rates settigns
     ois_data_name = "ois_bloomi_1w_30y.p"
     on_data_name = "overnight_rates.p"
@@ -37,46 +41,54 @@ if __name__ == "__main__":
 
     fx_data = pd.read_pickle(data_path+settings["fx_data_fixed"])
 
-    # Compute returns and spreads
-    spot = fx_data["spot_mid"].loc[currs, s_dt:e_dt, fixing_time]
-    spot_ret = np.log(spot).diff()
+    # Compute returns in bps
+    if "usd" in currs:
+        currs_x_usd = set(currs) - set(["usd"])
+        spot = fx_data["spot_mid"].loc[currs_x_usd, s_dt:e_dt, fixing_time]
+        spot_ret = np.log(spot).diff()
+        spot_ret["usd"] = - 1 * spot_ret.mean(axis=1) * 1e4
 
-    # Compute difference between OIS and overnight rates
-    ois_diff = (ois_data.rolling(5).mean() - on_data.rolling(5).mean())*1e2
+    else:
+
+        spot = fx_data["spot_mid"].loc[currs, s_dt:e_dt, fixing_time]
+        spot_ret = np.log(spot).diff() * 1e4
+
+    # Compute difference between OIS and overnight rates in bps
+    ois_diff = (ois_data.rolling(1).mean() - on_data.rolling(1).mean())*1e2
 
     # Estimation --------------------------------------------------------------
-    # Sample returns outside the event windows
+    # Get the stacked returns, and yield curve slopes; mark events classes
     eda = EventDataAggregator(events_data, spot_ret, pre, post)
     stacked_ret = eda.stack_data()
 
     eda_ois = EventDataAggregator(events_data, ois_diff.shift(10), pre, post)
     stacked_ois = eda_ois.stack_data()
 
-
+    # Fetch dummies for currency-specific fixed effects and event class
     dummies = pd.get_dummies(stacked_ret.drop(["data", "index"], axis=1))
+
     curr_fe = dummies[["asset_" + curr for curr in currs]]
+
     event_dummies = dummies[["event_" + evt_type for evt_type in
-                             ["hike", "cut"]]]
+                             ["hike", "cut", "no change"]]]
 
+    # OIS spread x event dummy
     interaction_terms = event_dummies.multiply(stacked_ois["data"], axis=0)
-    interaction_terms.columns = ["ois_x_hike", "ois_x_cut"]
+    interaction_terms.columns = ["ois_x_hike", "ois_x_cut", "ois_x_no_change"]
 
-    X = pd.concat([curr_fe, stacked_ois["data"].rename("ois"), event_dummies,
-                   interaction_terms],
-                   axis=1)
+    # Construct matrix of regressors
+    if include_currency_fe:
+        X = pd.concat([curr_fe, stacked_ois["data"].rename("ois"),
+                       event_dummies,interaction_terms], axis=1)
+    else:
+        X = pd.concat([stacked_ois["data"].rename("ois"), event_dummies,
+                       interaction_terms], axis=1)
 
-    ols = PureOls(y0=stacked_ret["data"]*1e4, X0=X, add_constant=False)
+    ols = PureOls(y0=stacked_ret["data"], X0=X,
+                  add_constant=not include_currency_fe)
     ols.fit()
-    ols.get_diagnostics(HAC=False)
 
-    # ret_data = stacked_ret.loc[stacked_ret["event"] == "none"].pivot(
-    #     index="index", columns="asset", values="data")
-    #
-    # # lol = taf.ts_ap_tests(ret_data*1e4,
-    # #                       ois_diff[["aud"]].shift(5)*1e2, 1)
-    #
-    # df = pd.get_dummies(stacked_ret)
-    # X = df.drop(["index", "data"], axis=1)
-    # X = df[["event_cut", "event_hike"]]
-    # y = df[["data"]]
-    # ols = sm.OLS(y, X)
+    res = ols.get_diagnostics(HAC=False)
+
+
+
